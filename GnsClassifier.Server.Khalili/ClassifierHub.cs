fc8afdacaefe8b7b1b60ac34f6a1a800b3e038c0
TMaster <github321>
2016-10-01 19:23:49 +0300

    Creating server layout and main services

---
+++ GnsClassifier.Server.Khalili/ClassifierHub.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using GnsClassifier.Common;
+using Microsoft.AspNet.SignalR;
+
+namespace GnsClassifier.Server.Khalili
+{
+    public class ClassifierHub : Hub
+    {
+        private readonly IDictionaryDb<string, ClassifierResult> _resultsDb;
+        private readonly IDictionaryDb<string, int> _contestDb;
+
+        public ClassifierHub()
+        {
+            var resultsDbLocation = ConfigurationManager.AppSettings["resultsFileDbLocation"];
+            var contestDbLocation = ConfigurationManager.AppSettings["contestFileDbLocation"];
+            _resultsDb = new DictionaryFileDb<string, ClassifierResult>(resultsDbLocation);
+            _contestDb = new DictionaryFileDb<string, int>(contestDbLocation);
+        }
+
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+
+        public string GetWordToClassify()
+        {
+            var words = GetUnknownWords();
+            if (!words.Any())
+            {
+                return "";
+            }
+            var randomizer = new Random();
+            var index = randomizer.Next(words.Count);
+            var word = words[index];
+            return word;
+        }
+
+
+
+        private IList<string> GetUnknownWords()
+        {
+            var wordsResults = _resultsDb.GetEntries();
+            var unknownWords = wordsResults
+                .Where(kvp => kvp.Value == ClassifierResult.Unknown)
+                .Select(kvp => kvp.Key).ToList();
+            return unknownWords;
+        }
+
+        public void SubmitClassifierResult(string word, ClassifierResult result)
+        {
+            if (string.IsNullOrEmpty(word))
+            {
+                return;
+            }
+            _resultsDb.UpdateEntry(word, result);
+            UpdateUserContextResults();
+        }
+
+
+
+        private void UpdateUserContextResults()
+        {
+          var userName = Context.User.Identity.Name;
+            var contestResults = _contestDb.GetEntries();
+            var userScore = 1;
+            if (contestResults.ContainsKey(userName))
+            {
+                userScore = contestResults[userName] + 1;
+            }
+            _contestDb.UpdateEntry(userName, userScore);
+        }
+    }
+}
